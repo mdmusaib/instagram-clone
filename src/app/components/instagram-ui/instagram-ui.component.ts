22bf5137d0dd748257d4cab71b9ceee511ebb004
Musaib <musaibkm@gmail.com>
2023-10-13 15:09:48 +0530

    Initial Commit for angular applicaiton
    created a Instagram clone applciation

---
+++ src/app/components/instagram-ui/instagram-ui.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app-instagram-ui',
+    templateUrl: './instagram-ui.component.html',
+    styleUrls: ['./instagram-ui.component.css'],
+
+})
+export class InstagramUI {
+
+}
